02b8295e9182813fc896d2ed5a739982f10328cb
Radim <radim.pavlicek@gmail.com>
2012-06-28 16:21:02 +0200

    move logic into other methdo

--- src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberRegistration.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/controller/MemberRegistration.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
-import javax.enterprise.event.Event;
 import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
@@ -16,49 +15,60 @@ import javax.persistence.EntityManager;
 import org.jboss.as.quickstarts.kitchensink.model.Member;
 
 import com.laurinka.skga.server.model.Result;
+import com.laurinka.skga.server.model.Snapshot;
+import com.laurinka.skga.server.model.SnapshotRun;
 import com.laurinka.skga.server.scratch.HCPChecker;
 import com.laurinka.skga.server.scratch.SkgaGolferNumber;
 
 // The @Stateful annotation eliminates the need for manual transaction demarcation
 @Stateful
-// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// The @Model stereotype is a convenience mechanism to make this a
+// request-scoped bean that has an
 // EL name
 // Read more about the @Model stereotype in this FAQ:
 // http://sfwk.org/Documentation/WhatIsThePurposeOfTheModelAnnotation
 @Model
 public class MemberRegistration {
 
-   @Inject
-   private Logger log;
+	@Inject
+	private Logger log;
 
-   @Inject
-   private FacesContext facesContext;
+	@Inject
+	private FacesContext facesContext;
 
-   @Inject
-   private EntityManager em;
+	@Inject
+	private EntityManager em;
 
-   @Inject
-   private Event<Member> memberEventSrc;
+	private Member newMember;
 
-   private Member newMember;
+	@Produces
+	@Named
+	public Member getNewMember() {
+		return newMember;
+	}
 
-   @Produces
-   @Named
-   public Member getNewMember() {
-      return newMember;
-   }
+	public void register() throws Exception {
+		SnapshotRun run = new SnapshotRun();
+		em.persist(run);
+		log.info("Starting scratching...");
 
-   public void register() throws Exception {
-      log.info("Registering " + newMember.getName());
-      em.persist(newMember);
-      Result r  = new HCPChecker().query(new SkgaGolferNumber(9811));
-      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!" + r.getName(), "Registration successful"));
-      memberEventSrc.fire(newMember);
-      initNewMember();
-   }
+		for (int i = 0; i < 15000; i++) {
+			Result query = new HCPChecker().query(new SkgaGolferNumber(i));
+			if (null == query) {
+				continue;
+			}
+			em.persist(new Snapshot(query, run));
+			log.info(query.toString());
+		}
+		log.info("End scratching...");
 
-   @PostConstruct
-   public void initNewMember() {
-      newMember = new Member();
-   }
+		facesContext.addMessage(null, new FacesMessage(
+				FacesMessage.SEVERITY_INFO, "Snapshoted!",
+				"Snapshot successful"));
+	}
+
+	@PostConstruct
+	public void initNewMember() {
+		newMember = new Member();
+	}
 }
