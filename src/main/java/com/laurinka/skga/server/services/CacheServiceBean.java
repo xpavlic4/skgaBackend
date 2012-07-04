2ded3ed9bc55d98f68af13e0fa2effd96d02322c
Radim <radim.pavlicek@gmail.com>
2012-07-04 11:39:23 +0200

    singleton cache service

--- src/main/java/com/laurinka/skga/server/services/CacheServiceBean.java
+++ src/main/java/com/laurinka/skga/server/services/CacheServiceBean.java
@@ -3,11 +3,11 @@ package com.laurinka.skga.server.services;
 import com.laurinka.skga.server.model.Result;
 import com.laurinka.skga.server.model.Snapshot;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-@ApplicationScoped
+@Singleton
 public class CacheServiceBean implements CacheService {
     @Inject
     EntityManager em;
