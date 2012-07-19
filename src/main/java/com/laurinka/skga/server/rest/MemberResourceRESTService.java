f837eb43cf79cb5ab93a17f4ecc7681a8773688d
Radim <radim.pavlicek@gmail.com>
2012-07-19 18:09:46 +0200

    list of names webservice

--- src/main/java/com/laurinka/skga/server/rest/MemberResourceRESTService.java
+++ src/main/java/com/laurinka/skga/server/rest/MemberResourceRESTService.java
@@ -1,53 +1,61 @@
 package com.laurinka.skga.server.rest;
 
-import com.laurinka.skga.server.model.Result;
-import com.laurinka.skga.server.model.SkgaNumber;
-import com.laurinka.skga.server.rest.model.Hcp;
-import com.laurinka.skga.server.scratch.SkgaGolferNumber;
-import com.laurinka.skga.server.services.SkgaWebsiteService;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.ws.rs.*;
-import java.util.List;
+import javax.persistence.NamedQuery;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+
+import com.laurinka.skga.server.model.Result;
+import com.laurinka.skga.server.rest.model.Hcp;
+import com.laurinka.skga.server.rest.model.NameNumberXml;
+import com.laurinka.skga.server.scratch.SkgaGolferNumber;
+import com.laurinka.skga.server.services.SkgaWebsiteService;
 
 /**
  * JAX-RS Example
  * <p/>
- * This class produces a RESTful service to read the contents of the members table.
+ * This class produces a RESTful service to read the contents of the members
+ * table.
  */
 @Path("/members")
 @RequestScoped
+@NamedQuery(name = "all", query = "select new com.laurinka.skga.server.rest.model.NameNumberXml(m.name, m.nr) from SkgaNumber m order by m.name")
 public class MemberResourceRESTService {
-    @Inject
-    private EntityManager em;
+	@Inject
+	private EntityManager em;
 
-    @Inject
-    private SkgaWebsiteService service;
+	@Inject
+	private SkgaWebsiteService service;
 
-    @GET
-    @Produces("text/xml")
-    public List<SkgaNumber> listAllMembers() {
-        String s = "select m from SkgaNumber m order by m.nr";
-        List<SkgaNumber> results = em.createQuery(s, SkgaNumber.class).getResultList();
-        return results;
-    }
+	@GET
+	@Produces("text/xml")
+	public List<NameNumberXml> listAllMembers() {
+		List<NameNumberXml> results = em.createNamedQuery("all", NameNumberXml.class)
+				.getResultList();
+		return results;
+	}
 
-    @GET
-    @Path("/{nr:[0-9][0-9]*}")
-    @Produces("text/xml")
-    public Hcp lookupMemberById(@PathParam("nr") String aNr) {
-        Result query = null;
-        query = service.findDetail(new SkgaGolferNumber(aNr));
-        if (null == query)
-            throw new WebApplicationException();
+	@GET
+	@Path("/{nr:[0-9][0-9]*}")
+	@Produces("text/xml")
+	public Hcp lookupMemberById(@PathParam("nr") String aNr) {
+		Result query = null;
+		query = service.findDetail(new SkgaGolferNumber(aNr));
+		if (null == query)
+			throw new WebApplicationException();
 
-        Hcp hcp = new Hcp();
-        hcp.setHandicap(query.getHcp());
-        hcp.setNumber(query.getSkgaNr());
-        hcp.setName(query.getName());
-        hcp.setClub(query.getClub());
-        return hcp;
-    }
+		Hcp hcp = new Hcp();
+		hcp.setHandicap(query.getHcp());
+		hcp.setNumber(query.getSkgaNr());
+		hcp.setName(query.getName());
+		hcp.setClub(query.getClub());
+		return hcp;
+	}
 }
---
+++ src/main/java/com/laurinka/skga/server/rest/model/NameNumberXml.java
@@ -0,0 +1,28 @@
+package com.laurinka.skga.server.rest.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class NameNumberXml {
+	private String name;
+	private String number;
+	
+	
+	public NameNumberXml(String name, String number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+}
