edc408e2d34427b0650cc13bb0d46e273663cf9e
Radim <radim.pavlicek@gmail.com>
2012-07-19 20:22:02 +0200

    test diacritics

--- src/main/java/com/laurinka/skga/server/controller/SyncingBean.java
+++ src/main/java/com/laurinka/skga/server/controller/SyncingBean.java
@@ -1,14 +1,17 @@
 package com.laurinka.skga.server.controller;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateful;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import com.laurinka.skga.server.job.NamesJob;
 import com.laurinka.skga.server.job.SkgaNumbersJob;
+import com.laurinka.skga.server.model.Configuration;
 
 @Stateful
 @Model
@@ -33,5 +36,16 @@ public class SyncingBean {
         log.info("updating names...start");
         namesJob.updateNames();
         log.info("updating names...end");
+    
+    }
+    
+    @Inject
+    EntityManager em;
+    
+    public void save() {
+    	Configuration entity = new Configuration();
+    	entity.setName(new Date().toString());
+    	entity.setValue("ĎURINĎÁK Ľuboš Ing.");
+		em.persist(entity);
     }
 }
