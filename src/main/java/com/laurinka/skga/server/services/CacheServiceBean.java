8348ab29af6264c8f813c17e5154705c10bcf7e5
Radim <radim.pavlicek@gmail.com>
2013-01-17 23:50:00 +0100

    cache on every query

--- src/main/java/com/laurinka/skga/server/services/CacheServiceBean.java
+++ src/main/java/com/laurinka/skga/server/services/CacheServiceBean.java
@@ -1,11 +1,11 @@
 package com.laurinka.skga.server.services;
 
-import com.laurinka.skga.server.model.Result;
-import com.laurinka.skga.server.model.Snapshot;
-
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
+import com.laurinka.skga.server.model.Result;
+import com.laurinka.skga.server.model.Snapshot;
 
 @Singleton
 public class CacheServiceBean implements CacheService {
@@ -14,6 +14,6 @@ public class CacheServiceBean implements CacheService {
 
     @Override
     public void cache(Result r) {
-        //em.persist(new Snapshot(r));
+        em.persist(new Snapshot(r));
     }
 }
