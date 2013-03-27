131a4635c86c16c244aa7e074259624cac24568d
Radim <radim.pavlicek@gmail.com>
2013-03-27 20:46:02 +0100

    remove reading from cache which is causing issues with accents

--- src/main/java/com/laurinka/skga/server/services/CachingSkgaWebsiteServiceBean.java
+++ src/main/java/com/laurinka/skga/server/services/CachingSkgaWebsiteServiceBean.java
@@ -1,17 +1,16 @@
 package com.laurinka.skga.server.services;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
 import com.google.common.base.Optional;
 import com.laurinka.skga.server.model.Result;
 import com.laurinka.skga.server.scratch.CgfGolferNumber;
 import com.laurinka.skga.server.scratch.CgfHCPChecker;
 import com.laurinka.skga.server.scratch.SkgaGolferNumber;
 import com.laurinka.skga.server.scratch.SkgaHCPChecker;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class CachingSkgaWebsiteServiceBean implements WebsiteService {
@@ -24,10 +23,10 @@ public class CachingSkgaWebsiteServiceBean implements WebsiteService {
     @Override
     public Result findDetail(SkgaGolferNumber nr) {
         try {
-        	Optional<Result> tmpRes = cache.find(nr);
-        	if (tmpRes.isPresent()) {
-        		return tmpRes.get();
-        	}
+//        	Optional<Result> tmpRes = cache.find(nr);
+//        	if (tmpRes.isPresent()) {
+//        		return tmpRes.get();
+//        	}
             Result result = new SkgaHCPChecker().query(nr);
             if (null != result)
                 cache.cache(result);
@@ -43,10 +42,10 @@ public class CachingSkgaWebsiteServiceBean implements WebsiteService {
     @Override
     public Result findDetail(CgfGolferNumber nr) {
         try {
-        	Optional<Result> tmpRes = cache.find(nr);
-        	if (tmpRes.isPresent()) {
-        		return tmpRes.get();
-        	}
+            Optional<Result> tmpRes = cache.find(nr);
+            if (tmpRes.isPresent()) {
+                return tmpRes.get();
+            }
             Result result = new CgfHCPChecker().query(nr);
             if (null != result)
                 cache.cache(result);
