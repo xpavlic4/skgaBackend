937cded3136224890e354c7c301f215575b42219
Radim Pavlicek <radim.pavlicek@gmail.com>
2013-06-27 09:59:24 +0200

    space on sql

--- src/main/java/com/laurinka/skga/server/rest/SearchMemberResourceRESTService.java
+++ src/main/java/com/laurinka/skga/server/rest/SearchMemberResourceRESTService.java
@@ -37,7 +37,7 @@ public class SearchMemberResourceRESTService {
     @Produces("text/xml")
     public List<NameNumberXml> lookupMemberById(@QueryParam("q") String q) {
         String[] split = REGEX.split(q);
-        String s2 = "and m.name2 like :name";
+        String s2 = " and m.name2 like :name";
         int i = 0;
         for (String part : split) {
             s2 += "and m.name2 like :name" + i;
