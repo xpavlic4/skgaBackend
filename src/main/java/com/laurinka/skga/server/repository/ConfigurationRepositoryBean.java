9c9297dc917041705911b3ee32f7bd4415ac38c7
Radim Pavlicek <radim.pavlicek@gmail.com>
2014-01-26 10:40:46 +0100

    undefined club created at the startup

--- src/main/java/com/laurinka/skga/server/repository/ConfigurationRepositoryBean.java
+++ src/main/java/com/laurinka/skga/server/repository/ConfigurationRepositoryBean.java
@@ -1,16 +1,22 @@
 package com.laurinka.skga.server.repository;
 
+import com.laurinka.skga.server.model.Club;
 import com.laurinka.skga.server.model.Configuration;
+import com.laurinka.skga.server.model.Result;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationScoped
+@Startup
 public class ConfigurationRepositoryBean implements ConfigurationRepository {
 
     @Inject
@@ -18,6 +24,10 @@ public class ConfigurationRepositoryBean implements ConfigurationRepository {
 
     @Inject
     Logger log;
+    /**
+     * Name of club that is used when club has not been been downloaded
+     */
+    private final String UNDEFINED = "Undefined";
 
     @Override
     public int getNumberOfNewNumbersToCheck() {
@@ -56,13 +66,30 @@ public class ConfigurationRepositoryBean implements ConfigurationRepository {
         em.persist(e2);
         em.flush();
 
-        {
-            Configuration e3 = new Configuration();
-            e3.setName(Keys.FIX_NAME_JOB.name());
-            e3.setValue("true");
-            em.persist(e3);
+        createFixMeFlag();
+        createNoClubEntry();
+    }
+
+    private void createNoClubEntry() {
+        TypedQuery<Club> namedQuery = em.createNamedQuery(Club.BYNAME, Club.class);
+        namedQuery.setParameter("name", UNDEFINED);
+        List<Club> resultList = namedQuery.getResultList();
+        if (resultList.size() == 0) {
+            log.log(Level.INFO, "Persisting logical club with name: {0} ", UNDEFINED);
+            Club club = new Club();
+            club.setName(UNDEFINED);
+            club.setType(Result.Type.CGF);
+            em.persist(club);
             em.flush();
         }
+    }
+
+    private void createFixMeFlag() {
+        Configuration e3 = new Configuration();
+        e3.setName(Keys.FIX_NAME_JOB.name());
+        e3.setValue("true");
+        em.persist(e3);
+        em.flush();
     }
 
     private Configuration findConfiguration(Keys key) {
