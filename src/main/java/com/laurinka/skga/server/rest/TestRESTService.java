fb3763706b8a0071cd4c4645b2db371b3a9b69a3
Radim <radim.pavlicek@gmail.com>
2014-05-19 19:27:56 +0200

    logentries

---
+++ src/main/java/com/laurinka/skga/server/rest/TestRESTService.java
@@ -0,0 +1,30 @@
+package com.laurinka.skga.server.rest;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * Just for testing and smoke tests.
+ */
+@Path("/test")
+@RequestScoped
+public class TestRESTService extends AbstractMemberResourceRestService {
+    private static Logger log = LogManager.getRootLogger();
+
+    @GET
+    @Path("/log")
+    public String testLogging() {
+        log.debug("I am a debug message");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "OK";
+    }
+
+}
