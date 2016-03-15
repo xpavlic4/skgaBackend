85d728a62063083f31df5c8f07a31a318a7d6d07
Radim Pavlicek <radim.pavlicek@bmi.gv.at>
2016-03-15 12:08:58 +0100

    lucia instead of radim in skga test

--- src/test/java/com/laurinka/skga/server/scratch/SkgaHCPCheckerTest.java
+++ src/test/java/com/laurinka/skga/server/scratch/SkgaHCPCheckerTest.java
@@ -10,7 +10,7 @@ public class SkgaHCPCheckerTest {
     @Test
     public void testQuery() throws Exception {
         final SkgaHCPChecker skgaHCPChecker = new SkgaHCPChecker();
-        final Result query = skgaHCPChecker.query(new SkgaGolferNumber(9811));
+        final Result query = skgaHCPChecker.query(new SkgaGolferNumber(9905));
         Logger.getAnonymousLogger().info(query.toString());
     }
 }
