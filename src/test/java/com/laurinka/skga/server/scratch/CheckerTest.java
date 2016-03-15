17d02ef684a04233d5b962f7c2726faf7503ea45
Radim Pavlicek <radim.pavlicek@bmi.gv.at>
2016-03-15 12:01:33 +0100

    test case changes (Lucia instead of Radim)

--- src/test/java/com/laurinka/skga/server/scratch/CheckerTest.java
+++ src/test/java/com/laurinka/skga/server/scratch/CheckerTest.java
@@ -1,5 +1,7 @@
 package com.laurinka.skga.server.scratch;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.text.Normalizer;
 import java.text.ParseException;
@@ -14,18 +16,18 @@ public class CheckerTest {
 
 	@Test
 	public void testMyHandicap() throws IOException, ParseException {
-		Result r  = new SkgaHCPChecker().query(new SkgaGolferNumber(9811));
-		Assert.assertTrue("HCP should be 28.4!", !r.getHcp().isEmpty());
-		Assert.assertEquals("SKGA Number should be 09811", r.getSkgaNr(),"09811");
+		Result r  = new SkgaHCPChecker().query(new SkgaGolferNumber("09905"));
+		assertTrue("HCP should be not empty!", !r.getHcp().isEmpty());
+		Assert.assertEquals("SKGA Number should be 09905", r.getSkgaNr(),"09905");
 		Assert.assertEquals("Club should be GC Pegas", r.getClub(), "GC Pegas");
-		Assert.assertEquals("Club should be PAVLÍČEK Radim", r.getName(), "PAVLÍČEK Radim");
+		assertTrue(r.getName().contains("Lucia"));
 
 	}
 	
 	@Test
 	public void testRandomHandicap() throws IOException, ParseException {
 		Result r  = new SkgaHCPChecker().query(new SkgaGolferNumber(99999));
-		Assert.assertTrue("Should be null as 1 not exists", r == null);
+		assertTrue("Should be null as 1 not exists", r == null);
 	}
 
 	@Test
